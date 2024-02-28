aae60518b7f12c33c858713b2802b96d52f195b7
Rozcowork <ethanorozco258@gmail.com>
2024-02-28 18:08:31 -0500

    Add Snake Controller and Food Destroyer

---
+++ Snake_CCNYEthan/Assets/Scripts/Snake.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    //GLOBAL VARIABLES
+    Vector3 dir = Vector3.right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MoveSnake", 0.3f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       ChangeDirection();
+    }
+
+    void MoveSnake()
+    {
+        transform.Translate(dir);
+    }
+
+    private void ChangeDirection()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir = Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir = Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir = Vector3.up;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir = Vector3.down;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Food")
+        {
+            //Debug.Log("food eaten");
+            Destroy(collision.gameObject);
+        }
+    }
+}
