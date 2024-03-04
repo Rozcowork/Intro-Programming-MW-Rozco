e2b56dd6092d84a3a60b8e25a1b19a3de11e7521
Rozcowork <ethanorozco258@gmail.com>
2024-03-04 18:01:11 -0500

    Added Tail to Snake
    Added code to make Tail longer after eating food

--- Snake_CCNYEthan/Assets/Scripts/Snake.cs
+++ Snake_CCNYEthan/Assets/Scripts/Snake.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Snake : MonoBehaviour
 {
     //GLOBAL VARIABLES
     Vector3 dir = Vector3.right;
 
+    //Keep Track of Tail Elements
+    List<Transform> tail = new List<Transform>();
+    bool ate = false;
+    public GameObject tailPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("MoveSnake", 0.3f, 0.3f);
+        InvokeRepeating("MoveSnake", 0.1f, 0.1f);
     }
 
     // Update is called once per frame
@@ -21,7 +27,25 @@ public class Snake : MonoBehaviour
 
     void MoveSnake()
     {
+        Vector3 gap = transform.position;
         transform.Translate(dir);
+
+        if (ate)
+        {
+            Debug.Log("Ate =" + ate);
+
+            GameObject tailSec = (GameObject)Instantiate(tailPrefab, gap, Quaternion.identity);
+            tail.Insert(0, tailSec.transform);
+            ate = false;
+        }
+
+
+        if (tail.Count > 0)
+        {
+            tail.Last().position = gap;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
     }
 
     private void ChangeDirection()
@@ -48,6 +72,8 @@ public class Snake : MonoBehaviour
     {
         if (collision.gameObject.tag == "Food")
         {
+            ate = true;
+
             //Debug.Log("food eaten");
             Destroy(collision.gameObject);
         }
