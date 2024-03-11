a5d47b870025e3796782b770bfe560f3d9b8bdd2
Rozcowork <ethanorozco258@gmail.com>
2024-03-11 17:57:04 -0400

    Created Scene Manager
    Learned how to change Scenes in Game

---
+++ PointAndClick_EthanCCNY/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // use scene library
+
+public class SceneChanger : MonoBehaviour
+{
+    //Global Variables
+    public int sceneNumber;
+    //0 = Start Scene
+    //1 = Main Scene
+    //2 = End Scene
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (sceneNumber == 0) //we are in the start scene
+        {
+            StartSceneControls();
+        }
+       else if (sceneNumber == 1) //we are in the main scene
+        {
+            MainSceneControls();
+        }
+       else if (sceneNumber == 2)
+        {
+            EndSceneControls();
+        }
+    }
+    public void StartSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))//if the enter key is pressed go to the mainscene
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+    public void MainSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+    public void EndSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+}
