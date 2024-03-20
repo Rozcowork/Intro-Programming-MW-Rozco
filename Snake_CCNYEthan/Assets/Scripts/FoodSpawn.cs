e4eda8238f40e3c101b34df10c2203f617271ab5
Rozcowork <ethanorozco258@gmail.com>
2024-03-20 18:03:01 -0400

    Created for Food to Spawn not in Walls

--- Snake_CCNYEthan/Assets/Scripts/FoodSpawn.cs
+++ Snake_CCNYEthan/Assets/Scripts/FoodSpawn.cs
@@ -39,8 +39,8 @@ public class FoodSpawn : MonoBehaviour
     }
     void RottenFoodSpawn()// spawn rotten food randomly
     {
-        int xPos = (int)Random.Range(wallLeft.position.x, wallRight.position.x); //Find a random position between the Left and right walls to spawn rotten food
-        int yPos = (int)Random.Range(wallTop.position.y, wallBottom.position.y); //Find a random position between the Top and bottom walls to spawn rotten food
+        int xPos = (int)Random.Range(wallLeft.position.x + 10, wallRight.position.x - 10); //Find a random position between the Left and right walls to spawn rotten food
+        int yPos = (int)Random.Range(wallTop.position.y - 10, wallBottom.position.y + 10); //Find a random position between the Top and bottom walls to spawn rotten food
 
         Instantiate(rottenfoodPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity); //Spawn rotten food in previously found position
     }
